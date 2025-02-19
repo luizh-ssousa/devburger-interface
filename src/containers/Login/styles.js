6462f3f70ea06e103dd14751d6e6af0d7888b171
Luiz Henrique Silva Sousa <luizh.ssousa@gmail.com>
2025-02-19 20:26:50 -0300

    styles

--- src/containers/Login/styles.js
+++ src/containers/Login/styles.js
@@ -1,10 +1,18 @@
 import styled from 'styled-components';
 
 
-export const Container = styled.div`
-    background: transparent;
-`;
+export const Container = styled.div``;
 
-export const Container = styled.div`
-    background: transparent;
-`;
+export const LeftContainer = styled.div``;
+
+export const RightContainer = styled.div``;
+
+export const Title = styled.h2``;
+
+export const Form = styled.form``;
+
+export const InputContainer = styled.div``;
+
+export const Link = styled.a``;
+
+export const Button = styled.button``;
