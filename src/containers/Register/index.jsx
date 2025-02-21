52c3dc34c299dc2363e457d18a0bd29a364332e2
Luiz Henrique Silva Sousa <luizh.ssousa@gmail.com>
2025-02-21 13:53:14 -0300

    update

--- src/containers/Register/index.jsx
+++ src/containers/Register/index.jsx
@@ -1,99 +1,110 @@
 import { useForm } from "react-hook-form";
 import { yupResolver } from '@hookform/resolvers/yup';
+import { useNavigate } from "react-router-dom";
 import { toast } from 'react-toastify';
 import * as yup from "yup";
-
 
 import Logo from '../../assets/logo.svg';
 import { Button } from '../../components/Button';
 import { api } from '../../services/api';
 import {
-    Container,
-    Form,
-    InputContainer,
-    LeftContainer,
-    RightContainer,
-    Title
+  Container,
+  Form,
+  InputContainer,
+  LeftContainer,
+  RightContainer,
+  Title,
+  Link,
 } from './styles';
 
 export function Register() {
-    const schema = yup
-        .object({
-            name: yup.string().required('O nome é obrigatório'),
-            email: yup
-                .string()
-                .email('Digite um e-mail válido')
-                .required('O e-mail é obrigatório'),
-            password: yup
-                .string()
-                .min(6, 'A senha deve ter pelo menos 6 caracteres')
-                .required('Digite uma senha'),
-            confirmPassword: yup
-                .string()
-                .oneOf([yup.ref('password')], 'As senhas devem ser iguais')
-                .required('Confirme sua senha'),
-        })
-        .required();
+  const navigate = useNavigate();
+  const schema = yup
+    .object({
+      name: yup.string().required('O nome é obrigatório'),
+      email: yup
+        .string()
+        .email('Digite um e-mail válido')
+        .required('O e-mail é obrigatório'),
+      password: yup
+        .string()
+        .min(6, 'A senha deve ter pelo menos 6 caracteres')
+        .required('Digite uma senha'),
+      confirmPassword: yup
+        .string()
+        .oneOf([yup.ref('password')], 'As senhas devem ser iguais')
+        .required('Confirme sua senha'),
+    })
+    .required();
 
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(schema),
+  });
 
-    const { register, handleSubmit, formState: { errors } } = useForm({
-        resolver: yupResolver(schema),
-    });
+  console.log(errors);
 
-    console.log(errors);
+  const onSubmit = async (data) => {
+    try {
+      const { status } = await api.post('/users', {
+        name: data.name,
+        email: data.email,
+        password: data.password,
+      },
+        {
+          validateStatus: () => true,
+        },
+      );
 
-    const onSubmit = async (data) => {
-        const { status } = await toast.promise(
-            api.post('/users', {
-                name: data.name,
-                email: data.email,
-                password: data.password,
-            }),
-            {
-                pending: 'Verificando seus dados',
-                success: 'Cadastro efetuado com Sucesso 👌',
-                error: 'Ops, algo deu errado! Tente novamente. 😲',
-            },
-        );
+      if (status === 200 || status === 201) {
+        setTimeout(() => {
+          navigate('/login');
+        }, 2000);
+        toast.success('Conta criada com sucesso!');
+      } else if (status === 409) {
+        toast.error('Email já cadastrado! Faça o login para continuar');
+      } else {
+        throw new Error();
+      }
+    } catch (error) {
+      toast.error('Falha no Sistema! Tente novamente');
+    }
+  };
 
-        console.log(response);
-    };
+  return (
+    <Container>
+      <LeftContainer>
+        <img src={Logo} alt='logo-devburger' />
+      </LeftContainer>
+      <RightContainer>
+        <Title>Criar Conta</Title>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <InputContainer>
+            <label>Nome</label>
+            <input type="text" {...register('name')} />
+            <p>{errors?.name?.message}</p>
+          </InputContainer>
+          <InputContainer>
+            <label>Email</label>
+            <input type="email" {...register('email')} />
+            <p>{errors?.email?.message}</p>
+          </InputContainer>
 
-    return (
-        <Container>
-            <LeftContainer>
-                <img src={Logo} alt='logo-devburger' />
-            </LeftContainer>
-            <RightContainer>
-                <Title>Criar Conta</Title>
-                <Form onSubmit={handleSubmit(onSubmit)}>
-                    <InputContainer>
-                        <label>Nome</label>
-                        <input type="text" {...register('name')} />
-                        <p>{errors?.name?.message}</p>
-                    </InputContainer>
-                    <InputContainer>
-                        <label>Email</label>
-                        <input type="email" {...register('email')} />
-                        <p>{errors?.email?.message}</p>
-                    </InputContainer>
-
-                    <InputContainer>
-                        <label>Senha</label>
-                        <input type="password" {...register('password')} />
-                        <p>{errors?.password?.message}</p>
-                    </InputContainer>
-                    <InputContainer>
-                        <label>Confirmar Senha</label>
-                        <input type="password" {...register('confirmPassword')} />
-                        <p>{errors?.confirmPassword?.message}</p>
-                    </InputContainer>
-                    <Button type="submit">Criar Conta</Button>
-                </Form>
-                <p>
-                    Já possui conta? <a>Clique aqui.</a>
-                </p>
-            </RightContainer>
-        </Container>
-    );
+          <InputContainer>
+            <label>Senha</label>
+            <input type="password" {...register('password')} />
+            <p>{errors?.password?.message}</p>
+          </InputContainer>
+          <InputContainer>
+            <label>Confirmar Senha</label>
+            <input type="password" {...register('confirmPassword')} />
+            <p>{errors?.confirmPassword?.message}</p>
+          </InputContainer>
+          <Button type="submit">Criar Conta</Button>
+        </Form>
+        <p>
+          Já possui conta? <Link to='/login'>Clique aqui.</Link>
+        </p>
+      </RightContainer>
+    </Container>
+  );
 }
--- src/containers/Register/styles.js
+++ src/containers/Register/styles.js
@@ -1,3 +1,4 @@
+import { Link as ReactLink } from 'react-router-dom';
 import styled from 'styled-components';
 import BackgroundLogin from '../../assets/background-login.svg';
 import Background from '../../assets/background.svg';
@@ -87,4 +88,9 @@ export const InputContainer = styled.div`
         font-weight: 600;
         height: 10px;
     }
+`;
+
+export const Link = styled(ReactLink)`
+    text-decoration: none;
+    color: #fff;
 `;
