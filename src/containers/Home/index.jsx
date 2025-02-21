f8c9fe07f372c6c69c24774dc291266cec323278
Luiz Henrique Silva Sousa <luizh.ssousa@gmail.com>
2025-02-21 14:22:59 -0300

    banner

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,7 +1,17 @@
+import { Banner, Container, Content } from './styles';
+
 export function Home() {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    );
+  return (
+    <main>
+      <Banner>
+        <h1>Bem-vindo(a)!</h1>
+      </Banner>
+      <Container>
+        <Content>
+          <div>Carrossel Categorias</div>
+          <div>Carrossel Produtos</div>
+        </Content>
+      </Container>
+    </main>
+  );
 }
