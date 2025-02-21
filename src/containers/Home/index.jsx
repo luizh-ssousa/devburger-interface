d5c595051f85b4f841c9acbab1accc58e13de9dd
Luiz Henrique Silva Sousa <luizh.ssousa@gmail.com>
2025-02-21 14:54:09 -0300

    token

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,3 +1,4 @@
+import { CategoriesCarousel } from '../../components/CategoriesCarousel';
 import { Banner, Container, Content } from './styles';
 
 export function Home() {
@@ -8,7 +9,7 @@ export function Home() {
       </Banner>
       <Container>
         <Content>
-          <div>Carrossel Categorias</div>
+          <CategoriesCarousel />
           <div>Carrossel Produtos</div>
         </Content>
       </Container>
