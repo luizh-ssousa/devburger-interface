e787772ca2a3fcaba60c82708bb660119832a0a8
Luiz Henrique Silva Sousa <luizh.ssousa@gmail.com>
2025-02-21 15:02:53 -0300

    responsividade

--- src/components/CategoriesCarousel/index.jsx
+++ src/components/CategoriesCarousel/index.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import Carousel from 'react-multi-carousel';
 import { api } from '../../services/api';
 
 export function CategoriesCarousel() {
@@ -6,13 +7,33 @@ export function CategoriesCarousel() {
 
   useEffect(() => {
     async function loadCategories() {
-      const response = await api.get('/categories')
+      const { data } = await api.get('/categories');
 
-      console.log(response);
+      setCategories(data);
+      console.log(data);
     }
 
     loadCategories();
   }, []);
+
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 4,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1280 },
+      items: 4,
+    },
+    tablet: {
+      breakpoint: { max: 1280, min: 690 },
+      items: 3,
+    },
+    mobile: {
+      breakpoint: { max: 690, min: 0 },
+      items: 2,
+    },
+  };
 
   return (
     <div>
